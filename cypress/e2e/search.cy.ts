1a4a611e10406d3ce766b149d5599a90f2a10096
Rafael Santana <rafaelsantana.devweb@gmail.com>
2024-11-18 21:42:05 -0300

    Adicionado testes e2e

---
+++ cypress/e2e/search.cy.ts
@@ -0,0 +1,24 @@
+describe('search products', () => {
+  it('should be able to search for products', () => {
+    cy.visit('/')
+
+    cy.get('input[name=q]').type('moletom').parent('form').submit()
+
+    cy.location('pathname').should('include', '/search')
+    cy.location('search').should('include', 'q=moletom')
+
+    cy.get('a[href^="/product"]').should('exist')
+  })
+
+  it('should not be able to visit search page without a search query', () => {
+    /** Comando usado para contornar possíveis exceptions não tratadas,
+     * como o redirecionamento realizado pelo Next
+     *
+     * cy.on('uncaught:exception', () => false)
+     */
+
+    cy.visit('/search')
+
+    cy.location('pathname').should('equal', '/')
+  })
+})
